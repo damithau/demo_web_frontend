1fa40387d6af7d7f6cb5b9bfa3d00d70a9add474
damithaudesh662 <damithavidanapathirana2000@gmail.com>
2024-12-16 14:31:35 +0530

    add all the ui validations

--- src/app/hotel-search/hotel-search.component.ts
+++ src/app/hotel-search/hotel-search.component.ts
@@ -6,19 +6,28 @@ import { NgFor, NgIf } from '@angular/common';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-hotel-search',
   templateUrl: './hotel-search.component.html',
   styleUrls: ['./hotel-search.component.css'],
-  imports:[FormsModule,NgFor,NgIf,CommonModule, MatExpansionModule, HttpClientModule,MatDividerModule],
+  imports: [
+    FormsModule,
+    NgFor,
+    NgIf,
+    CommonModule,
+    MatExpansionModule,
+    HttpClientModule,
+    MatDividerModule,
+  ],
   providers: [HotelSearchService],
-
 })
 export class HotelSearchComponent {
   checkInDate!: string;
   noOfNights!: number;
+  noOfNightsError: string = '';
+  CheckInDateError: string = '';
   roomRequests: { noOfRooms: number; noOfAdults: number }[] = [];
   searchResults: any[] = [];
   errorMessage: string = '';
@@ -34,11 +43,34 @@ export class HotelSearchComponent {
     this.roomRequests.splice(index, 1);
   }
 
+  validateInputs() {
+    const currentDate = new Date();
+    const checkInDate = new Date(this.checkInDate);
+
+    if (checkInDate < currentDate) {
+      console.log('Check-in date cannot be in the past');
+      this.CheckInDateError = 'Check-in date cannot be in the past';
+    } else {
+      this.CheckInDateError = '';
+    }
+
+    if (this.noOfNights < 1) {
+      console.log('Number of nights must be greater than 0');
+      this.noOfNightsError = 'Number of nights must be greater than 0';
+    } else {
+      this.noOfNightsError = '';
+    }
+  }
+
   searchHotels() {
+    this.validateInputs();
+    if (this.CheckInDateError || this.noOfNightsError) {
+      return;
+    }
     const searchCriteria = {
       checkInDate: this.checkInDate,
       noOfNights: this.noOfNights,
-      roomRequests: this.roomRequests
+      roomRequests: this.roomRequests,
     };
 
     this.hotelSearchService.searchHotels(searchCriteria).subscribe(
